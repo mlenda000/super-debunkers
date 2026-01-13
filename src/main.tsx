4849e04c57963cf5405556360dbbe82184cad807
Mark Lendacky <MLenda000@cable.comcast.com>
2026-01-13 14:53:42 -0500

    added hashrouter to the main.tsx to fix ghpage deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { GlobalProvider } from "./context";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
+    <HashRouter>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </HashRouter>
   </StrictMode>
 );
