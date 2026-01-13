0c88fb62fc4939be6e7ca4f2681753177c5e466f
Mark Lendacky <MLenda000@cable.comcast.com>
2026-01-13 16:28:08 -0500

    converted vite.config.ts back

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
-
 import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/super-debunkers/", // <-- IMPORTANT
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/super-debunkers/", // <-- IMPORTANT
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
