aefc4f814b699d42bcc1d93050b7bcff25077131
Mark Lendacky <MLenda000@cable.comcast.com>
2026-01-13 15:07:53 -0500

    was able to build fully locally

--- src/components/templates/gamePage/GamePage.tsx
+++ src/components/templates/gamePage/GamePage.tsx
@@ -1,20 +1,20 @@
 import RotateScreen from "@/components/atoms/rotateScreen/RotateScreen";
 import "./styles/game-page.css";
-import { useState } from "react";
+// import { useState } from "react";
 
 const GamePage = () => {
-  const [tacticCards, setTacticCards] = useState<any>();
-  const [newsCards, setNewsCards] = useState<any[]>([]);
-  const [activeNewsCard, setActiveNewsCard] = useState<any>(null);
-  const [gameRound, setGameRound] = useState<number>(1);
-  const [players, setPlayers] = useState<Player[]>([]);
-  const [showScoringModal, setShowScoringModal] = useState<boolean>(false);
-  const [showResponseModal, setShowResponseModal] = useState<any>(null);
-  const [showScoreCard, setShowScoreCard] = useState<boolean>(false);
-  const [roundEnd, setRoundEnd] = useState<boolean>(false);
-  const [roundStart, setRoundStart] = useState<boolean>(false);
-  const [finalRound, setFinalRound] = useState<boolean>(false);
-  const [endGame, setEndGame] = useState<boolean>(false);
+  //   const [tacticCards, setTacticCards] = useState<any>();
+  //   const [newsCards, setNewsCards] = useState<any[]>([]);
+  //   const [activeNewsCard, setActiveNewsCard] = useState<any>(null);
+  //   const [gameRound, setGameRound] = useState<number>(1);
+  //   const [players, setPlayers] = useState<Player[]>([]);
+  //   const [showScoringModal, setShowScoringModal] = useState<boolean>(false);
+  //   const [showResponseModal, setShowResponseModal] = useState<any>(null);
+  //   const [showScoreCard, setShowScoreCard] = useState<boolean>(false);
+  //   const [roundEnd, setRoundEnd] = useState<boolean>(false);
+  //   const [roundStart, setRoundStart] = useState<boolean>(false);
+  //   const [finalRound, setFinalRound] = useState<boolean>(false);
+  //   const [endGame, setEndGame] = useState<boolean>(false);
 
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,14 @@ import react from "@vitejs/plugin-react";
 //   },
 // });
 
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
   base: "/super-debunkers/", // <-- IMPORTANT
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
