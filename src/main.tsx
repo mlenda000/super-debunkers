59ba2836dadada0ef1ba09feb627c65f9e1f1829
Mark Lendacky <MLenda000@cable.comcast.com>
2026-01-13 16:32:04 -0500

    removed hash browsing from main and added vercel json

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { GlobalProvider } from "./context";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <GlobalProvider>
-        <App />
-      </GlobalProvider>
-    </HashRouter>
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </StrictMode>
 );
