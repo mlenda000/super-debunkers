ed600d64866fd9711f046c43acd373358a114275
Mark Lendacky <MLenda000@cable.comcast.com>
2026-01-15 08:55:47 -0500

    fixed web socket connection and have it properly putting everyone in the lobby until they choose a room

--- src/components/organisms/lobby/Lobby.tsx
+++ src/components/organisms/lobby/Lobby.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import {
   sendPlayerEnters,
   sendEnteredLobby,
+  initializeWebSocket,
 } from "@/services/webSocketService";
 
 import RoomTab from "@/components/atoms/roomTab/RoomTab";
@@ -14,23 +15,31 @@ const Lobby = ({ rooms }: { rooms: string[] }) => {
   const avatar = localStorage.getItem("avatarImage");
   const playerName = localStorage.getItem("playerName");
 
-  const handleClick = (name: string, room: string, avatarPath: string) => {
+  const handleClick = async (
+    name: string,
+    room: string,
+    avatarPath: string
+  ) => {
     const avatarName = avatarPath.substring(avatarPath.lastIndexOf("/") + 1);
     if (room === "Create room") {
       navigate("/game/create-room");
     } else {
+      await initializeWebSocket(room);
       sendPlayerEnters(room, { name, avatar: avatarName, room });
       navigate(`/game/${room}`);
     }
   };
 
   useEffect(() => {
-    const filteredRooms = rooms.find((room: string) => room !== "Create room");
-    const avatarName = avatar
-      ? avatar.substring(avatar.lastIndexOf("/") + 1)
-      : "";
-    sendEnteredLobby(filteredRooms, avatarName, playerName || "");
-  }, [rooms, avatar, playerName]);
+    const sendLobbyMessage = async () => {
+      const avatarName = avatar
+        ? avatar.substring(avatar.lastIndexOf("/") + 1)
+        : "";
+      await initializeWebSocket("lobby");
+      sendEnteredLobby("lobby", avatarName, playerName || "");
+    };
+    sendLobbyMessage();
+  }, [avatar, playerName]);
 
   return (
     <>
