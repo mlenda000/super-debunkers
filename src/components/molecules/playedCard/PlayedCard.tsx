ae61117477bae89bc6f75a8b6540e223ade9dc79
Mark Lendacky <MLenda000@cable.comcast.com>
2026-02-06 09:08:51 -0500

    refactored playedcard and replaced undo button

--- src/components/molecules/playedCard/PlayedCard.tsx
+++ src/components/molecules/playedCard/PlayedCard.tsx
@@ -1,35 +1,16 @@
-import { useState } from "react";
 import type { PlayedCardProps } from "@/types/types";
 
 const PlayedCard: React.FC<PlayedCardProps> = ({ name, image, id, onUndo }) => {
-  //ratio 2.5 : 3.5
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => setIsHovered(true);
-  const handleMouseLeave = () => setIsHovered(false);
-
   return (
-    <div
-      className="played-card"
-      id="playersHand"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      {isHovered && (
-        <button className="played-card__undo-icon" onClick={() => onUndo(id)}>
-          <img src={`/icons/undo-arrow-icon.svg`} alt="Undo" />
-        </button>
-      )}
-      <div className="played-card__content">
-        <div className="played-card__image-container">
-          <img
-            src={name === "The Truth" ? "/images/tactics/true.webp" : image}
-            alt={name}
-            className="played-card__image"
-            style={{ maxHeight: "250px", aspectRatio: "2.5 / 3.5" }}
-          />
-        </div>
-      </div>
+    <div className="card-container" id="playersHand">
+      <button className="close-button" onClick={() => onUndo(id)}>
+        <img src="/images/buttons/close.webp" alt="Undo" />
+      </button>
+      <img
+        src={name === "The Truth" ? "/images/tactics/true.webp" : image}
+        alt={name}
+        className="card-image"
+      />
     </div>
   );
 };
