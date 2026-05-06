b6ee59b97704f6e0c44bb00cccb6274a0c009be2
Mark Lendacky <MLenda000@cable.comcast.com>
2026-05-06 15:46:20 -0400

    updated news cards and tested look

--- src/components/templates/test/TestPage.tsx
+++ src/components/templates/test/TestPage.tsx
@@ -1,33 +1,36 @@
 import RoomTab from "@/components/atoms/roomTab/RoomTab";
 import "./styles/test.css";
 // import type { ThemeStyle } from "@/types/gameTypes";
-import Tool from "@/components/molecules/tool/Tool";
+// import Tool from "@/components/molecules/tool/Tool";
 import ButtonStyle from "@/components/atoms/buttonStyle/ButtonStyle";
 import Footer from "@/components/atoms/footer/Footer";
 // import ScoreModal from "@/components/organisms/modals/scoreModal/ScoreModal";
 // import EndGameModal from "@/components/organisms/modals/endGameModal/EndGameModal";
 // import PlayedCard from "@/components/molecules/playedCard/PlayedCard";
-// import NewsCard from "@/components/molecules/newsCard/NewsCard";
+import NewsCard from "@/components/molecules/newsCard/NewsCard";
 
-const mockInfluencer = {
-  caption: "this is a mock caption",
-  bodyCopy: "this is a mock body copy",
-  tacticUsed: [
-    // "emotional-manipulation",
-    // "conspiracy-theory",
-    // "gaslighting",
-    // "impersonation",
-    // "cherry-picking",
-    // "tricky-jokes",
-    // "fear-mongering",
-    // "deepfakes",
-    // "sock-puppetry",
-    // "clickbait",
-    "true",
-  ],
-  villain: "all" as ThemeStyle,
-  newsImage: "scientist.webp",
-};
+// const mockInfluencer = {
+//   caption: "this is a mock caption",
+//   bodyCopy: "this is a mock body copy",
+//   tacticUsed: [
+//     // "emotional-manipulation",
+//     // "conspiracy-theory",
+//     // "gaslighting",
+//     // "impersonation",
+//     // "cherry-picking",
+//     // "tricky-jokes",
+//     // "fear-mongering",
+//     // "deepfakes",
+//     // "sock-puppetry",
+//     // "clickbait",
+//     "true",
+//   ],
+//   villain: "all" as ThemeStyle,
+//   newsImage: "scientist.webp",
+// };
+
+const bodyCopy =
+  "This amazing vitamin makes you super smart! Tons of kids in the comments say it changed their lives, check them out! \n\nComments: \n\nFunkybird112: I love these vitamins, I am so smart now.\nFunkycat112: These vitamins made me smart.\nFunkyphish112: I am smarter with these vitamins.\nFunkydog112: These are the best vitamins for making you smart.";
 
 const TestPage = () => {
   return (
@@ -43,7 +46,7 @@ const TestPage = () => {
         }}
       /> */}
       {/* type?: "default" | "glass" | "glowing" | "outline" | "hover"; */}
-      <ButtonStyle type="glass" theme="all">
+      {/* <ButtonStyle type="glass" theme="all">
         <RoomTab
           room={"Create Room"}
           avatar={""}
@@ -55,7 +58,7 @@ const TestPage = () => {
             throw new Error("Function not implemented.");
           }}
         />
-      </ButtonStyle>
+      </ButtonStyle> */}
       {/* <Tool currentInfluencer={mockInfluencer} showResults /> */}
       {/* <PlayedCard
         name={"test"}
@@ -65,17 +68,13 @@ const TestPage = () => {
           throw new Error("Function not implemented.");
         }}
       /> */}
-      {/* <NewsCard
-        name={
-          "Kids with outdated phones are facing social isolation and depression at school! Parents must act NOW! 😢📱"
-        }
-        description={
-          "New studies show children with outdated technology experience devastating bullying and mental health! Don't let your child suffer social rejection—they need the latest iPhone to feel valued and included!"
-        }
-        category={["fear-mongering"]}
-        villain={"all"}
-        image={"celeb_influencers.webp"}
-      /> */}
+      <NewsCard
+        name={"New vitamin makes you super smart — everyone loves it! 🧠💊"}
+        description={bodyCopy}
+        category={["sock-puppetry"]}
+        villain={"bots"}
+        image={"bots_vitamin.webp"}
+      />
       <p className="test-text">Next next</p>
       <Footer />
     </div>
